e14519d39ca1abc032c0bd015a53eef85f441fdd
= <=>
2022-01-05 10:50:10 +0100

    lesson about the primite types in typescript

---
+++ src/types_lesson.ts
@@ -0,0 +1,24 @@
+console.log('Hello world!')
+
+// type number
+let num: number = 12
+let binary: number = 0b110
+
+// type string
+let firstName: string = 'Joel'
+let lastName: string = 'Mbengui'
+
+// type boolean
+let isLoading: boolean = false;
+isLoading = true
+
+// return Error
+// isLoading = 'false'
+
+let firstFivePrimes: number[] = [2, 3, 5, 7, 11]
+let firstFiverPrimes2: Array<number> = [2, 3, 5, 7, 11]
+
+console.log(firstFivePrimes)
+console.log(isLoading)
+console.log(firstName)
+console.log(binary)
